a840aa3f7a7c37a2044e90283ae2b0085fe2bb82
Thomas Alrek <thomas@alrek.no>
2020-01-20 08:53:48 +0100

    Move model logic into query builder

--- src/helpers.php
+++ src/helpers.php
@@ -13,3 +13,27 @@ if (!function_exists('has_trait')) {
     return in_array($trait, class_uses_recursive($subject), true);
   }
 }
+
+if (!function_exists('uuid')) {
+  /**
+   * Generates a unique id
+   *
+   * @return string
+   */
+  function uuid()
+  {
+    mt_srand(time());
+
+    return sprintf(
+      '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0x0fff) | 0x4000,
+      mt_rand(0, 0x3fff) | 0x8000,
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff)
+    );
+  }
+}
