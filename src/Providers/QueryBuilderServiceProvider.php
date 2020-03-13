a57139b20e9dd0c49096a15088d23fc0419da6a5
Thomas Alrek <thomas@alrek.no>
2020-03-13 11:16:01 +0100

    Add separate QueryAssoc facade

--- src/Providers/QueryBuilderServiceProvider.php
+++ src/Providers/QueryBuilderServiceProvider.php
@@ -15,6 +15,11 @@ class QueryBuilderServiceProvider extends ServiceProvider
   {
     $this->app->bind('QueryBuilder', function () {
       return (new Builder(false))
+        ->orderBy('id'); // Not all indexes has the default 'created' field
+    });
+
+    $this->app->bind('QueryBuilder.assoc', function () {
+      return (new Builder(false))
         ->assoc(true)
         ->orderBy('id'); // Not all indexes has the default 'created' field
     });
